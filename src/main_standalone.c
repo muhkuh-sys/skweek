36905f81549517e79efba40dd1a61106a19eccf5
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-08-02 14:48:13 +0200

    Do not setup the MMIOs - this will be done in the HBOOT image. Do not loop forever but exit after the tune is finished.

--- src/main_standalone.c
+++ src/main_standalone.c
@@ -9,22 +9,6 @@
 /*-------------------------------------------------------------------------*/
 
 
-static void init_mmios(void)
-{
-	HOSTDEF(ptAsicCtrlArea);
-	HOSTDEF(ptMmioCtrlArea);
-
-
-	/* Set MMIO4 to PIO. */
-	ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
-	ptMmioCtrlArea->aulMmio_cfg[4] = MMIO_CFG_PIO;
-
-	/* Set MMIO5 to GPIO4. */
-	ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
-	ptMmioCtrlArea->aulMmio_cfg[5] = MMIO_CFG_GPIO4;
-}
-
-
 #define XSTR(a) #a
 #define STR(a) XSTR(a)
 
@@ -35,31 +19,10 @@ extern const unsigned char TUNE_START[];
 extern const unsigned char TUNE_END[];
 
 
-void skweek_standalone(void) __attribute__((noreturn));
+void skweek_standalone(void);
 void skweek_standalone(void)
 {
-	unsigned long ulTimer;
-	int iElapsed;
-
-
-	systime_init();
-
-	/* Switch all LEDs off. */
-	rdy_run_setLEDs(RDYRUN_OFF);
-
-	init_mmios();
-
-	while(1)
-	{
-		/* Play the tune. */
-		skweek_play(TUNE_START, TUNE_END);
-
-		/* Delay for 5 seconds. */
-		ulTimer = systime_get_ms();
-		do
-		{
-			iElapsed = systime_elapsed(ulTimer, 5000);
-		} while( iElapsed==0 );
-	}
+	/* Play the tune. */
+	skweek_play(TUNE_START, TUNE_END);
 }
 
